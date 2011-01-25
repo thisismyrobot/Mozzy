3b124f640b212e69d6ab87d96addfbbbf45cf2ab
Robert Wallhead <rwallhead@gmail.com>
2011-01-25 14:05:14 +1100

    Initial commit of Mozzy

---
+++ eclipse/src/thisismyrobot/android/mozzy/Mozzy.java
@@ -0,0 +1,14 @@
+package thisismyrobot.android.mozzy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Mozzy extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
